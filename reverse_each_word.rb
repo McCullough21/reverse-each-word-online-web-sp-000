5a989d99a35532027cd52767ef075e0e8cd05d7d
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-19 19:30:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split
   reverse = []
-   array.map do |word|
+   array.collect do |word|
     reverse << word.reverse
   end
   reverse.join(" ")
